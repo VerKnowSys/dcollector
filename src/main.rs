29d4729230ecd7db34a367eb60e6627ccf93a4d6
Daniel (dmilith) Dettlaff <daniel@centra.com>
2022-03-08 14:55:58 +0100

    new: Implemented writing pid to file. Defined handling of Ctrl-C event.

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,11 @@ pub mod schema;
 pub mod ups;
 
 
-use std::{env, thread, time::Duration};
+use lockfile::Lockfile;
+use std::fs::remove_file;
+use std::io::Write;
+use std::process::exit;
+use std::{env, process, thread, time::Duration};
 
 use crate::{
     models::UpsStat,
@@ -23,10 +27,25 @@ use dotenv::dotenv;
 fn main() {
     dotenv().ok();
 
+    let pidfile = env::var("PID_FILE").unwrap_or_else(|_| String::from("dcollector.pid"));
+    let mut lockfile = Lockfile::create(&pidfile)
+        .unwrap_or_else(|_| panic!("Couldn't obtain lockfile: {}!", pidfile));
+    let pidstr = format!("{}", process::id());
+    lockfile
+        .write_all(pidstr.as_bytes())
+        .expect("Couldn't write pid to a lock file");
+    ctrlc::set_handler(move || {
+        remove_file(pidfile.clone()).unwrap_or_default();
+        println!("Interrupted…");
+        exit(1);
+    })
+    .expect("Error setting Ctrl-C handler");
+
     let sleep = env::var("SLEEP_SECONDS")
         .unwrap_or_else(|_| String::from("60"))
         .parse::<u64>()
         .unwrap_or(60);
+
     let pg_conn = establish_postgres_connection();
     loop {
         store_ups_entry(&pg_conn)
