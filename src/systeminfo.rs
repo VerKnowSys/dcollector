fe8cd044a35c6262dca3e1c2403279474e43f612
Daniel (dmilith) Dettlaff <daniel@me.com>
2022-09-19 10:11:53 +0200

    imp: More idiomatic / functional approach to sys_process_entries() and read_devices_list()

--- src/systeminfo.rs
+++ src/systeminfo.rs
@@ -41,46 +41,47 @@ pub fn sys_stats_entry(sys: &System) -> SysStat {
 /// Read and fill SysStat entry with stats from user processes
 #[instrument]
 pub fn sys_process_entries(sys: &System) -> Vec<ProcStat> {
-    let mut processes = vec![];
-    for process in sys.processes().values() {
-        let disk_usage = process.disk_usage();
+    sys.processes()
+        .values()
+        .into_iter()
+        .filter_map(|process| {
+            // Sleep 10ms to avoid time PK duplication with a lot of processes running in system:
+            thread::sleep(Duration::from_millis(10));
 
-        let maybe_time = UNIX_EPOCH + Duration::from_secs(process.start_time());
-        let start_time = if maybe_time == UNIX_EPOCH {
-            // if the time is the same as UNIX_EPOCH it means that the process is short lived
-            None
-        } else {
-            Some(maybe_time)
-        };
-        let proc_stat = ProcStat {
-            time: SystemTime::now(),
-            host_name: sys.host_name(),
-            exe: Some(process.exe().to_string_lossy().to_string()),
-            cmd: Some(process.cmd().join(" ")),
-            name: Some(process.name().to_string()),
-            disk_read: Some(disk_usage.read_bytes as i64),
-            disk_read_total: Some(disk_usage.total_read_bytes as i64),
-            disk_written: Some(disk_usage.written_bytes as i64),
-            disk_written_total: Some(disk_usage.total_written_bytes as i64),
-            cpu_usage: Some(process.cpu_usage()),
-            rss: Some(process.memory() as i64),
-            status: Some(process.status().to_string()),
-            start_time,
-        };
-        processes.push(proc_stat);
+            let disk_usage = process.disk_usage();
+            let maybe_time = UNIX_EPOCH + Duration::from_secs(process.start_time());
+            let start_time = if maybe_time == UNIX_EPOCH {
+                // if the time is the same as UNIX_EPOCH it means that the process is short lived
+                None
+            } else {
+                Some(maybe_time)
+            };
+            let exe = process.exe().to_string_lossy().to_string();
+            let name = process.name().to_string();
 
-        // Sleep 10ms to avoid time PK duplication with a lot of processes running in system:
-        thread::sleep(Duration::from_millis(10));
-    }
-    processes
+            if !exe.is_empty() && !name.is_empty() {
+                let proc_stat = ProcStat {
+                    time: SystemTime::now(),
+                    host_name: sys.host_name(),
+                    exe: Some(exe),
+                    cmd: Some(process.cmd().join(" ")),
+                    name: Some(name),
+                    disk_read: Some(disk_usage.read_bytes as i64),
+                    disk_read_total: Some(disk_usage.total_read_bytes as i64),
+                    disk_written: Some(disk_usage.written_bytes as i64),
+                    disk_written_total: Some(disk_usage.total_written_bytes as i64),
+                    cpu_usage: Some(process.cpu_usage()),
+                    rss: Some(process.memory() as i64),
+                    status: Some(process.status().to_string()),
+                    start_time,
+                };
+                Some(proc_stat)
+            } else {
+                None
+            }
+        })
+        .collect()
 }
-
-
-// #[cfg(target_os = "macos")]
-// let the_command = Command::new("sudo")
-//     .args(&["smartctl", "-j", "-f", "brief", "-A", &disk_device])
-//     .stdin(Stdio::null())
-//     .output();
 
 
 #[instrument]
@@ -114,70 +115,75 @@ fn read_devices_list() -> Vec<String> {
 #[instrument]
 /// Read and fill DiskStat entry with stats from the disks
 pub fn disk_stats_entry(sys: &System) -> Vec<DiskStat> {
-    let mut entries = vec![];
+    read_devices_list()
+        .into_iter()
+        .filter_map(|disk_device| {
+            let the_command = Command::new("smartctl")
+                .args(&["-j", "-f", "brief", "-A", &disk_device])
+                .stdin(Stdio::null())
+                .output();
 
-    for disk_device in read_devices_list() {
-        let the_command = Command::new("smartctl")
-            .args(&["-j", "-f", "brief", "-A", &disk_device])
-            .stdin(Stdio::null())
-            .output();
+            match the_command {
+                Ok(output) => {
+                    let data = String::from_utf8_lossy(&output.stdout).to_string();
+                    let smartctl_obj: Value =
+                        serde_json::from_str(data.as_str()).unwrap_or_default();
+                    trace!(
+                        "smartctl command successful, the parsed object: {smartctl_obj:#?}"
+                    );
 
-        match the_command {
-            Ok(output) => {
-                let data = String::from_utf8_lossy(&output.stdout).to_string();
-                let smartctl_obj: Value =
-                    serde_json::from_str(data.as_str()).unwrap_or_default();
-                trace!("smartctl command successful, the parsed object: {smartctl_obj:#?}");
+                    let mut disk_stat = DiskStat {
+                        name: Some(disk_device),
+                        host_name: sys.host_name(),
+                        temperature: Some(
+                            smartctl_obj["temperature"]["current"]
+                                .as_f64()
+                                .unwrap_or(0.0),
+                        ),
+                        ..DiskStat::default()
+                    };
 
-                let mut disk_stat = DiskStat {
-                    name: Some(disk_device),
-                    host_name: sys.host_name(),
-                    temperature: Some(
-                        smartctl_obj["temperature"]["current"]
-                            .as_f64()
-                            .unwrap_or(0.0),
-                    ),
-                    ..DiskStat::default()
-                };
+                    for attr in smartctl_obj["ata_smart_attributes"]["table"]
+                        .as_array()
+                        .expect("ata_smart_attributes.table should be retrievable")
+                    {
+                        if attr["name"] == "Seek_Error_Rate" {
+                            // seek_error_rate => Seek_Error_Rate
+                            disk_stat.seek_error_rate =
+                                Some(attr["raw"]["value"].as_i64().unwrap_or(0));
+                        }
 
-                for attr in smartctl_obj["ata_smart_attributes"]["table"]
-                    .as_array()
-                    .unwrap()
-                {
-                    if attr["name"] == "Seek_Error_Rate" {
-                        // seek_error_rate => Seek_Error_Rate
-                        disk_stat.seek_error_rate =
-                            Some(attr["raw"]["value"].as_i64().unwrap_or(0));
+                        if attr["name"] == "Throughput_Performance" {
+                            // throughput => Throughput_Performance
+                            disk_stat.throughput =
+                                Some(attr["raw"]["value"].as_i64().unwrap_or(0));
+                        }
+
+                        if attr["name"] == "Raw_Read_Error_Rate" {
+                            // read_error_rate => Raw_Read_Error_Rate
+                            disk_stat.read_error_rate =
+                                Some(attr["raw"]["value"].as_i64().unwrap_or(0));
+                        }
+
+                        if attr["name"] == "UDMA_CRC_Error_Count" {
+                            // crc_errors => UDMA_CRC_Error_Count
+                            disk_stat.crc_errors =
+                                Some(attr["raw"]["value"].as_i64().unwrap_or(0));
+                        }
+
+                        if attr["name"] == "Seek_Time_Performance" {
+                            // seek_time => Seek_Time_Performance
+                            disk_stat.seek_time =
+                                Some(attr["raw"]["value"].as_i64().unwrap_or(0));
+                        }
                     }
-
-                    if attr["name"] == "Throughput_Performance" {
-                        // throughput => Throughput_Performance
-                        disk_stat.throughput =
-                            Some(attr["raw"]["value"].as_i64().unwrap_or(0));
-                    }
-
-                    if attr["name"] == "Raw_Read_Error_Rate" {
-                        // read_error_rate => Raw_Read_Error_Rate
-                        disk_stat.read_error_rate =
-                            Some(attr["raw"]["value"].as_i64().unwrap_or(0));
-                    }
-
-                    if attr["name"] == "UDMA_CRC_Error_Count" {
-                        // crc_errors => UDMA_CRC_Error_Count
-                        disk_stat.crc_errors =
-                            Some(attr["raw"]["value"].as_i64().unwrap_or(0));
-                    }
-
-                    if attr["name"] == "Seek_Time_Performance" {
-                        // seek_time => Seek_Time_Performance
-                        disk_stat.seek_time = Some(attr["raw"]["value"].as_i64().unwrap_or(0));
-                    }
+                    Some(disk_stat)
                 }
-
-                entries.push(disk_stat);
+                Err(err) => {
+                    error!("smartctl failed with: {err}");
+                    None
+                }
             }
-            Err(err) => error!("smartctl failed with: {err}"),
-        }
-    }
-    entries
+        })
+        .collect()
 }
